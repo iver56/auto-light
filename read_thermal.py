1493a34170cf88e2748185cd6b242ba7803b93c8
iver56 <iver56@hotmail.com>
2015-10-18 19:10:25 +0200

    After activation, don't turn off light when human moves

--- read_thermal.py
+++ read_thermal.py
@@ -32,7 +32,7 @@ pigpio_relay_pin = 4
 handle = pi.i2c_open(1, 0x0a)  # open Omron D6T device at address 0x0a on bus 1
 
 previous_celsius_data = []
-last_stationary_human_detected = datetime.datetime.now() - datetime.timedelta(minutes=10)
+time_stationary_detected = datetime.datetime.now() - datetime.timedelta(minutes=10)
 
 desired_light_level = 0
 current_light_level = 0.0
@@ -94,7 +94,7 @@ def turn_light_off():
 
 
 def tick(i2c_bus, OMRON_1, data):
-    global previous_celsius_data, last_stationary_human_detected
+    global previous_celsius_data, time_stationary_detected
 
     i2c_bus.write_byte(OMRON_1, 0x4c)
     (bytes_read, data) = pi.i2c_read_device(handle, len(data))
@@ -107,11 +107,14 @@ def tick(i2c_bus, OMRON_1, data):
 
     stationary = is_stationary_human(celsius_data)
     moving = is_moving_human(celsius_data, previous_celsius_data)
+    now = datetime.datetime.now()
     
     if stationary and not moving:
-        last_stationary_human_detected = datetime.datetime.now()
+        time_stationary_detected = now
         turn_light_on()
-    elif last_stationary_human_detected >= datetime.datetime.now() - datetime.timedelta(seconds=3):
+    elif moving and time_stationary_detected >= now - datetime.timedelta(seconds=10):
+        turn_light_on()
+    elif time_stationary_detected >= now - datetime.timedelta(seconds=3):
         turn_light_on()
     else:
         turn_light_off()
